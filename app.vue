76e3773b430c416a1a40f07b5f00701257c089c8
Flavio Guerra <flavio.guerra@globalsys.com.br>
2024-08-27 17:22:12 -0300

    meta pixel, uma coisa muito simples pra não funcionar direito

--- app.vue
+++ app.vue
@@ -27,9 +27,35 @@ useHead({
 				.join('\n')}`,
 		},
 	],
+	script:[
+		{ innerHTML: `!function(f,b,e,v,n,t,s)
+				{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+				n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+				if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+				n.queue=[];t=b.createElement(e);t.async=!0;
+				t.src=v;s=b.getElementsByTagName(e)[0];
+				s.parentNode.insertBefore(t,s)}(window, document,'script',
+				'https://connect.facebook.net/en_US/fbevents.js');
+				fbq('init', '939506908180273');
+				fbq('track', 'PageView');
+				`
+		}
+	],
+	noscript:[
+		{children:`<img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=939506908180273&ev=PageView&noscript=1"
+		/>`,
+		
+ },
+		
+	]
+
+	
 });
+
+
 </script>
-<template>
+<template>	
 	<NuxtLayout>
 		<NuxtLoadingIndicator
 			color="repeating-linear-gradient(to right,#FF99DD
